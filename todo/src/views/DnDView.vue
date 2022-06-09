30d69b19b488a0d4e2bfc327e2fefa97aa200a74
Tofu <osawa.shuto@gmail.com>
2022-06-10 00:37:25 +0900

    grids are added

--- todo/src/views/DnDView.vue
+++ todo/src/views/DnDView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app" class="bg-gray-200items-center">
-  <vue-draggable-next tag="ul" ghost-class="moving-card" filter=".action-button" class="flex flex-row flex-wrap w-full max-w-md" :list="users" :animation="200">
+  <div id="app" class="container mx-auto bg-gray-200items-center">
+  <vue-draggable-next tag="ul" ghost-class="moving-card" filter=".action-button" class="grid gap-4 grid-cols-3 w-full max-w-lg" :list="users" :animation="200">
     <div v-for="user in users"
          :key="user.id"
          class=" w-40 h-40 bg-white shadow-md rounded m-2 flex justify-center items-center text-lg text-gray-700"

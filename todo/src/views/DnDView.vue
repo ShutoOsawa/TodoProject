55e188e3f8dd1898576f9420dac3537cfac79c5c
Tofu <osawa.shuto@gmail.com>
2022-06-13 00:44:37 +0900

    card fixed

--- todo/src/views/DnDView.vue
+++ todo/src/views/DnDView.vue
@@ -8,28 +8,28 @@
 
     <vue-draggable-next tag="ul" ghost-class="moving-card"
                         filter=".action-button"
-                        class="grid gap-4 grid-cols-3 w-full max-w-lg"
-                        :list="lst" :animation="200">
-            <div  v-for="(item,index) in lst"
+                        class="grid gap-4 grid-cols-3 w-full max-w-4xl"
+                        :list="currentList" :animation="200">
+            <div  v-for="(item,index) in currentList"
              :key=index
-             class="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm"
+             class="w-48 h-54 bg-white shadow-md border border-gray-200 rounded-lg"
              >
-              <img class="rounded-t-lg" :src="item.image" alt="">
-              <p class="text-gray-700 text-base">
+              <img class="mx-auto my-4 h-32 bg-gray-100 rounded-lg" :src="item.image" alt="">
+              <p class="truncate px-2 text-gray-700 text-base">
                 {{item.name}}
               </p>
-
-            <button class="px-2 text-red-600"
-                    @click="removeCategory(index)"
-                    title="Remove todo">&times;</button>
-            <button @click="changeData(index)"
-                    class="px-2 text-green-600" title="Mark as done">&check;</button>
+              <div class="flex flex-row-reverse">
+                  <TrashIcon class="h-10 w-10 px-2 text-red-600"
+                          @click="removeCategory(index)"
+                          />
+                  <PencilAltIcon @click="changeData(index)"
+                          class="h-10 w-10 px-2 text-green-600" />
+              </div>
           </div>
         </vue-draggable-next>
     </div>
 
-  <div class="home">
-    <h1>DropZone</h1>
+  <div class="dropzone">
     <DropZone @drop.prevent="drop" @change="selectedFile" />
   </div>
 </template>
@@ -37,55 +37,57 @@
 <script>
 import DropZone from "@/components/DropZone.vue";
 import {defineComponent ,ref,onMounted} from "vue";
-
 import { VueDraggableNext } from 'vue-draggable-next';
-
+import { PencilAltIcon,TrashIcon } from '@heroicons/vue/solid'
 export default defineComponent({
-  components: {VueDraggableNext,
-  DropZone},
+  components: {
+    VueDraggableNext,
+  DropZone,
+  PencilAltIcon,
+  TrashIcon},
   setup() {
-
-    let lst = ref([])
-    let category = ref([])
+    let currentList = ref([])
+    let originalTree = ref([])
 
     onMounted(() => {
-      lst.value = category.value
+      currentList.value = originalTree.value
     })
 
     function returnButton() {
-      lst.value = category.value
+      currentList.value = originalTree.value
     }
 
     function removeCategory(index) {
       console.log(index)
-      lst.value.splice(index, 1);
+      currentList.value.splice(index, 1);
     }
 
     function changeData(index) {
-      lst.value = category.value[index].content
+      currentList.value = originalTree.value[index].content
     }
 
     const drop = (e) => {
       Array.from(e.dataTransfer.files).forEach(item => {
         console.log(item)
-        lst.value.push({id: item.name, name: item.name, image: URL.createObjectURL(item), content: []})
+        currentList.value.push({id: item.name, name: item.name, image: URL.createObjectURL(item), content: []})
       })
     };
 
     const selectedFile = () => {
        Array.from(document.querySelector(".dropzoneFile").files).forEach(item => {
         console.log(item)
-        lst.value.push({id: item.name, name: item.name, image: URL.createObjectURL(item), content: []})
+        currentList.value.push({id: item.name, name: item.name, image: URL.createObjectURL(item), content: []})
       })
     }
 
       return {
-        drop, selectedFile,
-        lst,
+        drop,
+        selectedFile,
+        currentList,
+        originalTree,
         returnButton,
         changeData,
         removeCategory,
-        category,
       }
     }
   })
